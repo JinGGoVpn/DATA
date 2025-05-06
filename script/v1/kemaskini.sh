2d157e8648af9d9ab3da821c1b10fad8ff829f0b
JINGGOVPN <114797590+JinGGoVpn@users.noreply.github.com>
2025-05-06 09:33:20 -0700

    Update kemaskini.sh

--- script/v1/kemaskini.sh
+++ script/v1/kemaskini.sh
@@ -7,10 +7,12 @@ websc=https://raw.githubusercontent.com/JinGGoVPN/DATA/main
 rm -f /usr/local/bin/add-xvless
 rm -f /usr/local/bin/trial-xvless
 rm -f /usr/local/bin/menu
+rm -f /usr/local/bin/mwarp
 # download script
 cd /usr/local/bin
 wget -O add-xvless "${websc}/script/xray/add-xvless.sh" && chmod +x add-xvless
 wget -O trial-xvless "${websc}/script/xray/trial-xvless.sh" && chmod +x trial-xvless
 wget -O menu "${websc}/script/v1/menu.sh" && chmod +x menu
+wget -O mwarp "${websc}/script/warp/mwarp.sh" && chmod +x mwarp
 cd
 clear

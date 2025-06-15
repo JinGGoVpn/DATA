f4cf2455a2fba73e5fe88bbeff3c2ccc51e30714
JINGGOVPN <114797590+JinGGoVpn@users.noreply.github.com>
2025-06-14 23:10:10 -0700

    Add files via upload

--- script/sshws/websocket.sh
+++ script/sshws/websocket.sh
@@ -8,25 +8,6 @@ cd
 # // GIT USER
 websc=https://raw.githubusercontent.com/JinGGoVPN/DATA/main
 
-# // SYSTEM WEBSOCKET HTTPS 2092
-cat <<EOF> /etc/systemd/system/ws-https.service
-[Unit]
-Description=Python Proxy Mod By JinGGo
-Documentation=https://github.com/JinGGo/
-After=network.target nss-lookup.target
-
-[Service]
-Type=simple
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-Restart=on-failure
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-https 
-
-[Install]
-WantedBy=multi-user.target
-EOF
 
 # // SYSTEM WEBSOCKET HTTP 8880
 cat <<EOF> /etc/systemd/system/ws-http.service
@@ -48,8 +29,6 @@ WantedBy=multi-user.target
 EOF
 
 
-# // PYTHON WEBSOCKET TLS && NONE
-wget -q -O /usr/local/bin/ws-https ${websc}/script/sshws/ws-https; chmod +x /usr/local/bin/ws-https
 
 # // PYTHON WEBSOCKET DROPBEAR
 wget -q -O /usr/local/bin/ws-http ${websc}/script/sshws/ws-http; chmod +x /usr/local/bin/ws-http

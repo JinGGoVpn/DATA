a90d246958872d740ba72197c44a5918f6540d8b
JINGGOVPN <114797590+JinGGoVpn@users.noreply.github.com>
2025-11-28 05:03:51 +0800

    Update kemaskini.sh

--- script/lifetime/upgrade/kemaskini.sh
+++ script/lifetime/upgrade/kemaskini.sh
@@ -9,6 +9,8 @@ rm -f /usr/local/bin/trial-xvless
 rm -f /usr/local/bin/trial-xvless
 rm -f /usr/local/bin/vless-list
 rm -f /usr/local/bin/menu
+rm -f /usr/local/bin/status
+
 #buat file
 touch /usr/local/bin/vless-list
 chmod +x /usr/local/bin/vless-list
@@ -19,6 +21,7 @@ wget -O add-xvless "${websc}/script/lifetime/upgrade/add-xvless.sh" && chmod +x 
 wget -O trial-xvless "${websc}/script/lifetime/upgrade/trial-xvless.sh" && chmod +x trial-xvless
 wget -O menu "${websc}/script/lifetime/upgrade/menu.sh" && chmod +x menu
 wget -O vless-list "${websc}/script/lifetime/upgrade/vless-list.sh" && chmod +x vless-list
+wget -O status "${websc}/script/lifetime/upgrade/status.sh" && chmod +x status
 
 
 cd

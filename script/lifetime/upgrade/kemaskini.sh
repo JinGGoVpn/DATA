f7f53fdc54068d4d82bd3216bc9511e294aeaa95
JINGGOVPN <114797590+JinGGoVpn@users.noreply.github.com>
2025-12-02 11:12:05 +0800

    Update kemaskini.sh

--- script/lifetime/upgrade/kemaskini.sh
+++ script/lifetime/upgrade/kemaskini.sh
@@ -7,11 +7,15 @@ websc=https://raw.githubusercontent.com/JinGGoVPN/DATA/main
 rm -f /usr/local/bin/add-xvless
 rm -f /usr/local/bin/trial-xvless
 rm -f /usr/local/bin/add-host
+rm -f /usr/local/bin/del-xvless
+rm -f /usr/local/bin/renew-xvless
 
 # download script
 cd /usr/local/bin
 wget -O add-xvless "${websc}/script/lifetime/upgrade/add-xvless.sh" && chmod +x add-xvless
 wget -O trial-xvless "${websc}/script/lifetime/upgrade/trial-xvless.sh" && chmod +x trial-xvless
+wget -O renew-xvless "${websc}/script/lifetime/upgrade/renew-xvless.sh" && chmod +x renew-xvless
+wget -O del-xvless "${websc}/script/lifetime/upgrade/del-xvless.sh" && chmod +x del-xvless
 wget -O add-host "${websc}/script/lifetime/upgrade/add-host.sh" && chmod +x add-host
 
 

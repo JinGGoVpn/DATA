43de0cd6b03e62c738f5b78cc795ecde8f2b4ee8
JINGGOVPN <114797590+JinGGoVpn@users.noreply.github.com>
2025-12-18 15:06:55 +0800

    Update kemaskini.sh

--- script/lifetime/upgrade/kemaskini.sh
+++ script/lifetime/upgrade/kemaskini.sh
@@ -6,19 +6,20 @@ websc=https://raw.githubusercontent.com/JinGGoVPN/DATA/main
 #delete file
 rm -f /usr/local/bin/add-xvless
 rm -f /usr/local/bin/trial-xvless
-rm -f /usr/local/bin/add-host
 rm -f /usr/local/bin/del-xvless
 rm -f /usr/local/bin/renew-xvless
-rm -f /usr/local/bin/mdns
+rm -f /usr/local/bin/menu
+rm -f /usr/local/bin/recert-xray
+rm -f /usr/local/bin/vless-list
+
+
 
 # download script
 cd /usr/local/bin
-wget -O add-xvless "${websc}/script/lifetime/upgrade/add-xvless.sh" && chmod +x add-xvless
-wget -O trial-xvless "${websc}/script/lifetime/upgrade/trial-xvless.sh" && chmod +x trial-xvless
-wget -O renew-xvless "${websc}/script/lifetime/upgrade/renew-xvless.sh" && chmod +x renew-xvless
-wget -O del-xvless "${websc}/script/lifetime/upgrade/del-xvless.sh" && chmod +x del-xvless
-wget -O add-host "${websc}/script/lifetime/upgrade/add-host.sh" && chmod +x add-host
-wget -O mdns "${websc}/script/lifetime/upgrade/mdns.sh" && chmod +x mdns
+wget -O mxray "${websc}/script/lifetime/upgrade/mxray.sh" && chmod +x mxray
+wget -O mssh "${websc}/script/lifetime/upgrade/mssh.sh" && chmod +x mssh
+wget -O mbckp "${websc}/script/lifetime/upgrade/mbckp.sh" && chmod +x mbckp
+wget -O menu "${websc}/script/lifetime/upgrade/menu.sh" && chmod +x menu
 
 
 cd

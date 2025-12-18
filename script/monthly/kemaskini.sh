b1a94aef9fcf1b369932b653a2c38949a49c09ab
JINGGOVPN <114797590+JinGGoVpn@users.noreply.github.com>
2025-12-18 12:13:54 +0800

    Update kemaskini.sh

--- script/monthly/kemaskini.sh
+++ script/monthly/kemaskini.sh
@@ -4,11 +4,9 @@ clear
 websc=https://raw.githubusercontent.com/JinGGoVPN/DATA/main
 
 #delete file
-rm -f /usr/local/bin/add-xvless
-rm -f /usr/local/bin/trial-xvless
+rm -f /usr/local/bin/mxray
 # download script
 cd /usr/local/bin
-wget -O add-xvless "${websc}/script/lifetime/upgrade/add-xvless.sh" && chmod +x add-xvless
-wget -O trial-xvless "${websc}/script/lifetime/upgrade/trial-xvless.sh" && chmod +x trial-xvless
+wget -O monthly "${websc}/script/monthly/mxray.sh" && chmod +x mxray
 cd
 clear

0cd99bed2b7e28385ea68dbe45b963fa575b7131
JINGGOVPN <114797590+JinGGoVpn@users.noreply.github.com>
2025-12-18 15:26:52 +0800

    Update kemaskini.sh

--- script/lifetime/upgrade/kemaskini.sh
+++ script/lifetime/upgrade/kemaskini.sh
@@ -11,7 +11,7 @@ rm -f /usr/local/bin/renew-xvless
 rm -f /usr/local/bin/menu
 rm -f /usr/local/bin/recert-xray
 rm -f /usr/local/bin/vless-list
-
+rm -f /usr/local/bin/mwarp
 
 
 # download script
@@ -20,7 +20,7 @@ wget -O mxray "${websc}/script/lifetime/upgrade/mxray.sh" && chmod +x mxray
 wget -O mssh "${websc}/script/lifetime/upgrade/mssh.sh" && chmod +x mssh
 wget -O mbckp "${websc}/script/lifetime/upgrade/mbckp.sh" && chmod +x mbckp
 wget -O menu "${websc}/script/lifetime/upgrade/menu.sh" && chmod +x menu
-
+wget -O mwarp "${websc}/script/lifetime/upgrade/mwarp.sh" && chmod +x mwarp
 
 cd
 clear

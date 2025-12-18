af93020b6cb585a597dd2dd7376ce4993969bc42
JINGGOVPN <114797590+JinGGoVpn@users.noreply.github.com>
2025-12-18 13:05:14 +0800

    Update kemaskini.sh

--- script/monthly/kemaskini.sh
+++ script/monthly/kemaskini.sh
@@ -5,8 +5,12 @@ websc=https://raw.githubusercontent.com/JinGGoVPN/DATA/main
 
 #delete file
 rm -f /usr/local/bin/mxray
+rm -f /usr/local/bin/menu
+rm -f /usr/local/bin/mssh
 # download script
 cd /usr/local/bin
-wget -O monthly "${websc}/script/monthly/mxray.sh" && chmod +x mxray
+wget -O mxray "${websc}/script/monthly/mxray.sh" && chmod +x mxray
+wget -O menu "${websc}/script/monthly/menu.sh" && chmod +x menu
+wget -O mssh "${websc}/script/monthly/mssh.sh" && chmod +x mssh
 cd
 clear

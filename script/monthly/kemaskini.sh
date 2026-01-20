e041c2a3a31a00bd1045ad96b30a0e5df4291ab9
JINGGOVPN <114797590+JinGGoVpn@users.noreply.github.com>
2026-01-21 02:50:54 +0800

    Update kemaskini.sh

--- script/monthly/kemaskini.sh
+++ script/monthly/kemaskini.sh
@@ -9,7 +9,7 @@ rm -f /usr/local/bin/menu
 rm -f /usr/local/bin/mssh
 # download script
 cd /usr/local/bin
-wget -O mxray "${websc}/script/monthly/mxray.sh" && chmod +x mxray
+wget -O mxray "${websc}/script/lifetime/upgrade/mxray.sh" && chmod +x mxray
 wget -O menu "${websc}/script/monthly/menu.sh" && chmod +x menu
 wget -O mssh "${websc}/script/monthly/mssh.sh" && chmod +x mssh
 cd

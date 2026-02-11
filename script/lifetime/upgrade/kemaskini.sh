86f924ff67b102a54f9c1cd55913a19af45bff62
JINGGOVPN <114797590+JinGGoVpn@users.noreply.github.com>
2026-02-11 08:28:18 +0800

    Update kemaskini.sh

--- script/lifetime/upgrade/kemaskini.sh
+++ script/lifetime/upgrade/kemaskini.sh
@@ -4,23 +4,12 @@ clear
 websc=https://raw.githubusercontent.com/JinGGoVPN/DATA/main
 
 #delete file
-rm -f /usr/local/bin/add-xvless
-rm -f /usr/local/bin/trial-xvless
-rm -f /usr/local/bin/del-xvless
-rm -f /usr/local/bin/renew-xvless
-rm -f /usr/local/bin/menu
-rm -f /usr/local/bin/recert-xray
-rm -f /usr/local/bin/vless-list
-rm -f /usr/local/bin/mwarp
+rm -f /usr/local/bin/mxray
 
 
 # download script
 cd /usr/local/bin
 wget -O mxray "${websc}/script/lifetime/upgrade/mxray.sh" && chmod +x mxray
-wget -O mssh "${websc}/script/lifetime/upgrade/mssh.sh" && chmod +x mssh
-wget -O mbckp "${websc}/script/lifetime/upgrade/mbckp.sh" && chmod +x mbckp
-wget -O menu "${websc}/script/lifetime/upgrade/menu.sh" && chmod +x menu
-wget -O mwarp "${websc}/script/lifetime/upgrade/mwarp.sh" && chmod +x mwarp
 
 cd
 clear

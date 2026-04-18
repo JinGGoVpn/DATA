c2bf77cf4b16ab89e8c2a703103429386af8819b
JINGGOVPN <114797590+JinGGoVpn@users.noreply.github.com>
2026-04-18 09:33:55 +0800

    Update kemaskini.sh

--- script/lifetime/upgrade/kemaskini.sh
+++ script/lifetime/upgrade/kemaskini.sh
@@ -4,11 +4,13 @@ clear
 websc=https://raw.githubusercontent.com/JinGGoVPN/DATA/main
 
 #delete file
+rm -f /usr/local/bin/menu
 rm -f /usr/local/bin/mxray
 rm -f /usr/local/bin/mport-xray
 
 # download script
 cd /usr/local/bin
+wget -O menu "${websc}/script/lifetime/upgrade/menu.sh" && chmod +x menu
 wget -O mxray "${websc}/script/lifetime/upgrade/mxray.sh" && chmod +x mxray
 wget -O mport-xray "${websc}/script/lifetime/upgrade/mport-xray.sh" && chmod +x mport-xray
 cd
